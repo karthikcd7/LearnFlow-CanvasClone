55afc1818dce2c7a2f45feccbebd5e2c6153f5e7
karthikcd7 <karthikcd7@gmail.com>
2023-10-19 23:26:28 -0400

    update lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,10 +1,24 @@
-import Javascript from './Javascript';
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import JavaScript from "./Javascript";
+
 function Assignment3() {
-    return (
-        <div>
-            <h1>Assignment 3</h1>
-            <Javascript />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <JavaScript />
+      <PathParameters />
+      <Styles />
+      <Classes />
+      <TodoItem />
+      <TodoList />
+      <ConditionalOutput />
+      
+    </div>
+  );
 }
 export default Assignment3;
