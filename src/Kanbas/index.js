a34ef730397f12f0d74a28fccbfdb7f00c4f3d2a
karthikcd7 <karthikcd7@gmail.com>
2023-12-02 01:24:04 -0500

    assignment 6 until signup

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,9 +1,11 @@
 import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Signin from "./Users/signin";
+import Account from "./Users/account";
+import UserTable from "./Users/table";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import Assignments from "./Courses/Assignments";
-import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import "./css/menu.css";
@@ -17,7 +19,10 @@ function Kanbas() {
         <div class="col" style={{ paddingRight: 20 }}>
           <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="account" element={<Account />} />
+            <Route path="/account/:id" element={<Account />} />
+            <Route path="/admin/users" element={<UserTable />} />
             <Route path="Dashboard" element={<Dashboard />} />
             <Route path="Courses/" element={<Dashboard />} />
             <Route path="Courses/:courseId/*" element={<Courses />} />
